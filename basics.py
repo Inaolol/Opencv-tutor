1bb58465b5c6c6ceb12ec995b8f6561e9922fcd9
Abdirizak Olol <abdirisakmcm@gmail.com>
2024-03-07 17:13:53 +0300

    Updated

--- basics.py
+++ basics.py
@@ -1,18 +1,18 @@
 import cv2
 
-img = cv2.imread("Opencv/pic.jpg", -1)
+img = cv2.imread("Opencv-tutor/pic.jpg", -1)
 
-# -1,  cv2.IMREAD_COLOR : Loads a color image, Any transparency of image will be neglected. It is the default flag.
-# 0,  cv2.IMREAD_GRAYSCALE : Loads image in greyscale mode
-# 1,  cv2.IMREAD_UNCHANGED : Loads image as such including alpha channel
+# -1: cv2.IMREAD_COLOR - Loads a color image (ignores transparency)
+# 0: cv2.IMREAD_GRAYSCALE - Loads image in grayscale mode
+# 1: cv2.IMREAD_UNCHANGED - Loads image as-is, including alpha channel
 
 
 # Resize the image and rotate
-img = cv2.resize(img, (0, 0), fx=0.2, fy=0.3)
-img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
+img = cv2.resize(img, (0, 0), fx=0.3, fy=0.2)
+img = cv2.rotate(img, cv2.ROTATE_180)
 
 # SAVE THE IMAGE or 'WRITE'
-cv2.imwrite("Opencv/new_img.jpg", img)
+cv2.imwrite("Opencv-tutor/new_img.jpg", img)
 
 # display image
 cv2.imshow("image", img)
